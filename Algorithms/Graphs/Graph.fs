6389e2af24d2857efa952471164eab0925ee9330
mivuorin <mikko.h.vuorinen@gmail.com>
2022-06-17 20:26:07 +0300

    Edges to graph

--- Algorithms/Graphs/Graph.fs
+++ Algorithms/Graphs/Graph.fs
@@ -1,16 +1,41 @@
 ﻿namespace Graphs
 
 type Node = string
-type Graph = Map<Node, Node list>
 
+type Edge = (Node * Node)
+
+type Edges = Set<Node>
+
+type Graph = Map<Node, Set<Node>>
 
 module Graph =
-    let create adjacentList =
-        Map adjacentList
-    let node n = n
-    
-    let nodes values =
-        values
-        |> List.map node
 
-    let edges node (graph: Graph) = graph.[node]
+    let create (adjacentList: seq<Node * Node list>) : Graph =
+        adjacentList
+        |> Seq.map (fun (key, nodes) -> (key, Set nodes))
+        |> Map
+
+    let empty: Graph = create []
+
+    let node n = n
+
+    let nodes values = values |> List.map node
+
+    let edges node (graph: Graph) = graph.[node] |> Set.toList
+
+    let fromEdges (edges: Edge list) =
+        let addOrPrepend node (existing: Edges option) =
+            match existing with
+            | Some existing -> Set.add node existing |> Some
+            | None -> Set [ node ] |> Some
+
+        let rec loop edges graph =
+            match edges with
+            | (source, destination) :: rest ->
+                graph
+                |> Map.change source (addOrPrepend destination)
+                |> Map.change destination (addOrPrepend source)
+                |> loop rest
+            | [] -> graph
+
+        loop edges empty
---
+++ Algorithms/Graphs/UndirectedPath.fs
@@ -0,0 +1,34 @@
+﻿module Graphs.UndirectedPath
+
+open System
+open FsUnit
+open NUnit.Framework
+
+let i = Graph.node "i"
+let j = Graph.node "j"
+let k = Graph.node "k"
+let m = Graph.node "m"
+let l = Graph.node "l"
+let o = Graph.node "o"
+let n = Graph.node "n"
+
+let edges: Edge list =
+    [ (i, j)
+      (k, i)
+      (m, k)
+      (k, l)
+      (o, n) ]
+
+// unidirectional cyclic adjacent list
+let expected =
+    Graph.create [ (i, [ j; k ])
+                   (j, [ i ])
+                   (k, [ i; m; l ])
+                   (m, [ k ])
+                   (l, [ k ])
+                   (o, [ n ])
+                   (n, [ o ]) ]
+
+[<Test>]
+let Edges_to_graph () =
+    edges |> Graph.fromEdges |> should equal expected
